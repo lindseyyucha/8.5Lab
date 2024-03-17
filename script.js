8cb19fa914af0a1b700d34acba40e55a211af70c
lindseyyucha <119362898+lindseyyucha@users.noreply.github.com>
2024-03-17 21:47:15 +0000

    wip

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const supabaseUrl = 'https://hcqebjnptjgxiilhbajh.supabase.co';
-const supabaseKey = 'YOUR_SUPABASE_KEY_HERE';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcWViam5wdGpneGlpbGhiYWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyNzAxNzUsImV4cCI6MjAyNTg0NjE3NX0.a-bx7aMAkGA2QjRPa3zoObjV5EoQmnYZraMHlGFIN38';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function getBooks() {
